5c5bb27ec1e97f39e99f740248e6b3cf72087817
H <hedi.hadiji@gmail.com>
2015-12-03 16:12:19 +0000

    écrire train set

--- machine_learning.py
+++ machine_learning.py
@@ -35,40 +35,40 @@ class Learn:
     nx = 20
     ny = 20
     n_cell = nx * ny
-    n_coups = 4
+    n_coups = 5
     coups_sautes = 60
 
     def __init__(self, new=False, display=False):
-        self.possibilities=generate(Learn.n_coups)
+        self.possibilities = generate(Learn.n_coups)
         self.explore = 0.8
         self.jeu = MJ.Jeu(autorepeat=False, display=display)
         self.jeu.restart(Learn.coups_sautes)
         self.image = self.get_image()
         if new:
-            self.nn = Regressor(layers=[Layer("Linear", units=(Learn.n_cell+Learn.n_coups)),Layer("Linear", units=800), Layer("Sigmoid")], learning_rate=0.1, n_iter=1)
-            self.nn.fit(self.good_shape(self.possibilities[Learn.n_coups/2 - 1]), np.array([[0]]))
+            self.nn = Regressor(layers=[Layer("Linear", units=(Learn.n_cell+Learn.n_coups)),Layer("Sigmoid", units=800), Layer("Softmax")], learning_rate=0.1, n_iter=1)
+            self.nn.fit(self.good_shape(self.image, self.possibilities[Learn.n_coups/2 - 1]), np.array([[0]]))
         else:
             self.nn = pickle.load(open('nn.pkl', 'rb'))
-            self.nn.fit(self.good_shape(self.possibilities[Learn.n_coups/2 - 1]), np.array([[1]]))
+            self.nn.fit(self.good_shape(self.image, self.possibilities[Learn.n_coups/2 - 1]), np.array([[1]]))
 
-    def good_shape(self, instructions):#instructions est un tableau de 0 et 1 à 7 éléments
+
+    def good_shape(self, image, instructions):#instructions est un tableau de 0 et 1 à 7 éléments
         tab = np.zeros((1, (Learn.n_cell + Learn.n_coups)))
-        a = self.image
-        tab[0, ::] = np.append(a.flatten(), instructions)
-        return tab
+        tab[0, ::] = np.append(image.flatten(), instructions)
+        return 10 * tab
 
 
-    def learn(self, num_iter=1000, disp_mean_t=False,print_auc=False):
+    def learn(self, num_iter=1000, disp_mean_t=False, print_auc=False):
         predicted_outcome = np.zeros(2**Learn.n_coups)
         t = 0.
         n_morts = 0
         for s in xrange(num_iter):
-            if (s%100==0):
+            if (s%100 == 0):
                 print s
             outcome = 1
             for i, elt in enumerate(self.possibilities):
-                predicted_outcome[i] = self.nn.predict(self.good_shape(elt))
-            print predicted_outcome
+                predicted_outcome[i] = self.nn.predict(self.good_shape(self.image, elt))
+            #print predicted_outcome
             i = np.argmax(predicted_outcome)
             r = rd.random()
             if (r < self.explore):
@@ -87,7 +87,7 @@ class Learn:
                             print t, self.jeu.temps
                         self.jeu.restart(Learn.coups_sautes)
             self.image = self.get_image()
-            tab = self.good_shape(self.possibilities[i])
+            tab = self.good_shape(self.image, self.possibilities[i])
             self.nn.fit(tab, np.array([[outcome]]))
             if (s % 1000 == 0):
                 if print_auc:
@@ -104,7 +104,7 @@ class Learn:
             poss = self.possibilities
             i = rd.randint(0, len(poss)-1)
             elt = poss[i]
-            predicted_outputs.append(self.nn.predict(self.good_shape(elt))[0])
+            predicted_outputs.append(self.nn.predict(self.good_shape(self.image, elt))[0])
             r = rd.random()
             if (r < 0.8):
                 i = int(rd.random() * 2**(Learn.n_coups))
@@ -121,6 +121,7 @@ class Learn:
             self.image = self.get_image()        
         fpr, tpr, thresholds = metrics.roc_curve(real_outputs, predicted_outputs)
         return metrics.auc(fpr, tpr)
+    
     
     def benchmark(self, num_iter=1000):
         temps_total = []
@@ -141,6 +142,7 @@ class Learn:
         self.image = self.get_image()
         print str(sum(temps_total)*1. / num_iter) + " +/- "+ str(0.96 * np.sqrt(np.var(np.array(temps_total)) / num_iter)  )
 
+
     def get_image(self):
         nx_im, ny = 2 * Learn.nx, Learn.ny
         tab = np.ones((nx_im, ny))/2.
@@ -152,21 +154,60 @@ class Learn:
                 tab[int(nx_im * x_p) % nx_im, int(ny * y_m)] = 1
         x = 0.5 * nx_im
         y = y * ny
+        #t = " "
         for i in xrange(2):
             for j in xrange(2):
                 tab[x+i-1, y+j-1] = 0
-#        for i in xrange (nx_im/2):
+#        for i in xrange (nx_im / 2):
 #            print t
-#            t=" "
+#            t = " "
 #            for j in xrange(ny):
-#                t+=" " + str(tab[10+i][j])
+#                t += " " + str(tab[10+i][j])
         return tab[10:30, ::]
 
-    def set_display(self, bool):
-        self.display=bool
+    def set_display(self, boolean):
+        self.display = boolean
         self.jeu = MJ.Jeu(autorepeat=False, display=self.display)
         self.jeu.restart(Learn.coups_sautes)
         self.image = self.get_image()
 
-learn=Learn(new=True, display=True)
-learn.learn(num_iter=10000, disp_mean_t=False)
+    def save_rd_train_set(self, num_iter=10000, overwrite=False, train_file=None): # returns a set of situations, choice sequences, and outcomes
+        self.jeu.restart(Learn.coups_sautes)        
+        if train_file:
+            train_set = pickle.load(open(train_file, "rb" ))
+        else:
+            train_set = pickle.load(open( "train_set.csv", "rb" ))
+        if overwrite:
+            train_set = []
+        for i in xrange(num_iter):
+            im = self.image
+            choice = self.possibilities[rd.randint(0, 2**Learn.n_coups-1)]
+            outcome = 1
+            for elt in choice:
+                if (outcome == 1):
+                    if elt:
+                        instr = 'd'
+                    else:
+                        instr = 'q'
+                    if (self.jeu.update_all(instr) == "Dead"):
+                        outcome = 0
+                        self.jeu.restart(Learn.coups_sautes)
+            train_set.append((im, choice, outcome))
+        pickle.dump(train_set, open("train_set.csv", "wb"))
+        if train_file:
+            pickle.dump(train_set, open(train_file, "wb"))
+        else:
+            pickle.dump(train_set, open("train_set.csv", "wb"))
+        return
+
+    def intensive_train(self, train_file=None): # trainfile is a string
+        if train_file:
+            train_set = pickle.load(open(train_file, "rb"))
+        else:
+            train_set = pickle.load(open("train_set.csv", "rb" ))
+        for training in train_set:
+            im, choice, outcome = training
+            self.nn.fit(self.good_shape(image, choice), np.array([[outcome]]))
+        
+learn = Learn(new=True, display=True)
+#learn.learn(num_iter=10000, disp_mean_t=False)
