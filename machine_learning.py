d09c4ff4ba3a225cf33356cebdf2b1d55df862d1
maxime.louis <maxam33@hotmail.fr>
2015-12-03 11:03:34 +0000

    Quelques motifs.

--- machine_learning.py
+++ machine_learning.py
@@ -35,18 +35,18 @@ class Learn:
     nx = 20
     ny = 20
     n_cell = nx * ny
-    n_coups = 8
+    n_coups = 4
     coups_sautes = 60
 
     def __init__(self, new=False, display=False):
         self.possibilities=generate(Learn.n_coups)
-        self.explore = 0.2
+        self.explore = 0.8
         self.jeu = MJ.Jeu(autorepeat=False, display=display)
         self.jeu.restart(Learn.coups_sautes)
         self.image = self.get_image()
         if new:
-            self.nn = Regressor(layers=[Layer("Sigmoid", units=(Learn.n_cell+Learn.n_coups)),Layer("Sigmoid", units=300), Layer("Sigmoid")], learning_rate=0.05, n_iter=1)
-            self.nn.fit(self.good_shape(self.possibilities[Learn.n_coups/2 - 1]), np.array([[1]]))
+            self.nn = Regressor(layers=[Layer("Linear", units=(Learn.n_cell+Learn.n_coups)),Layer("Linear", units=800), Layer("Sigmoid")], learning_rate=0.1, n_iter=1)
+            self.nn.fit(self.good_shape(self.possibilities[Learn.n_coups/2 - 1]), np.array([[0]]))
         else:
             self.nn = pickle.load(open('nn.pkl', 'rb'))
             self.nn.fit(self.good_shape(self.possibilities[Learn.n_coups/2 - 1]), np.array([[1]]))
@@ -58,14 +58,17 @@ class Learn:
         return tab
 
 
-    def learn(self, num_iter=1000, disp_mean_t=False):
+    def learn(self, num_iter=1000, disp_mean_t=False,print_auc=False):
         predicted_outcome = np.zeros(2**Learn.n_coups)
         t = 0.
         n_morts = 0
         for s in xrange(num_iter):
+            if (s%100==0):
+                print s
             outcome = 1
             for i, elt in enumerate(self.possibilities):
                 predicted_outcome[i] = self.nn.predict(self.good_shape(elt))
+            print predicted_outcome
             i = np.argmax(predicted_outcome)
             r = rd.random()
             if (r < self.explore):
@@ -86,8 +89,11 @@ class Learn:
             self.image = self.get_image()
             tab = self.good_shape(self.possibilities[i])
             self.nn.fit(tab, np.array([[outcome]]))
-        pickle.dump(self.nn, open('nn.pkl', 'wb'))
-        print "NN Saved"
+            if (s % 1000 == 0):
+                if print_auc:
+                    print  "New auc is : ", learn.auc(num_iter=10000)
+                pickle.dump(self.nn, open('nn.pkl', 'wb'))
+                print "NN Saved"
         
         
     def auc(self, num_iter=10000):
@@ -96,19 +102,19 @@ class Learn:
         for s in xrange(num_iter):
             outcome = 1
             poss = self.possibilities
-            i = rd.randint(0, len(poss))
+            i = rd.randint(0, len(poss)-1)
             elt = poss[i]
-            predicted_outputs.append(self.nn.predict(self.good_shape(elt)))
+            predicted_outputs.append(self.nn.predict(self.good_shape(elt))[0])
             r = rd.random()
-            if (r < 1):
+            if (r < 0.8):
                 i = int(rd.random() * 2**(Learn.n_coups))
-            for elt in possibilities[i]:
+            for elt in self.possibilities[i]:
                 if (outcome == 1):
                     if elt:
                         instr = 'd'
                     else:
                         instr = 'q'
-                    if (jeu.update_all(instr) == "Dead"):
+                    if (self.jeu.update_all(instr) == "Dead"):
                         outcome = 0
                         self.jeu.restart(Learn.coups_sautes)
             real_outputs.append(outcome)
@@ -133,11 +139,11 @@ class Learn:
                     self.jeu.restart()
                     break
         self.image = self.get_image()
-        print str(sum(temps_total) / num_iter.) + " +/- "+ str(0.96 * np.sqrt(np.var(np.array(temps_total)) / num_iter)  )
+        print str(sum(temps_total)*1. / num_iter) + " +/- "+ str(0.96 * np.sqrt(np.var(np.array(temps_total)) / num_iter)  )
 
     def get_image(self):
         nx_im, ny = 2 * Learn.nx, Learn.ny
-        tab = np.zeros((nx_im, ny))
+        tab = np.ones((nx_im, ny))/2.
         x, y = self.jeu.joueur.position
         for elt in self.jeu.missiles:
             x_m, y_m = elt.position
@@ -146,46 +152,21 @@ class Learn:
                 tab[int(nx_im * x_p) % nx_im, int(ny * y_m)] = 1
         x = 0.5 * nx_im
         y = y * ny
-        for i in xrange(5):
-            for j in xrange(5):
-                tab[x+i-5, y+j-5] = -1
+        for i in xrange(2):
+            for j in xrange(2):
+                tab[x+i-1, y+j-1] = 0
+#        for i in xrange (nx_im/2):
+#            print t
+#            t=" "
+#            for j in xrange(ny):
+#                t+=" " + str(tab[10+i][j])
         return tab[10:30, ::]
 
+    def set_display(self, bool):
+        self.display=bool
+        self.jeu = MJ.Jeu(autorepeat=False, display=self.display)
+        self.jeu.restart(Learn.coups_sautes)
+        self.image = self.get_image()
 
-#==============================================================================
-# predicted_outputs = []
-# real_outputs = []
-# 
-# for s in xrange(num_iter):
-#     #for i, elt in enumerate(possibilities):
-#         #predicted_outcome[i] = nn.predict(good_shape(jeu, elt))
-#     #i = np.argmax(predicted_outcome)
-#     r = rd.random()
-#     if (r < 1):
-#         i = int(rd.random() * 2**(n_coups))
-#     outcome = 1
-#     predicted_outcome = nn.predict(good_shape(jeu, possibilities[i]))
-#     for elt in possibilities[i]:
-#         if (outcome == 1):
-#             if (jeu.update_all() == "Dead"):
-#                 restart_for_learning()
-#             if elt:
-#                 instr = 'd'
-#             else:
-#                 instr = 'q'
-#             if (jeu.update_all(instr) == "Dead"):
-#                 restart_for_learning()
-#     tab = good_shape(jeu, possibilities[i])
-#     predicted_outputs.append(predicted_outcome[0])
-#     real_outputs.append(outcome)
-#     #nn.fit(tab, np.array([[outcome]]))
-# 
-# 
-# fpr, tpr, thresholds = metrics.roc_curve(real_outputs, predicted_outputs)
-# print metrics.auc(fpr, tpr)
-# 
-# 
-# 
-# pickle.dump(nn, open('nn.pkl', 'wb'))
-# print "Saved"
-#==============================================================================
+learn=Learn(new=True, display=True)
+learn.learn(num_iter=10000, disp_mean_t=False)
